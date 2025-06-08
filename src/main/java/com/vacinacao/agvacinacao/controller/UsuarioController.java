8531156c287e0d1ab725b59bf9ec9e7a77a1fa37
Lealca10 <leandro.alcantara14@gmail.com>
2025-06-08 20:14:53 -0300

    Adição recuperação de senha

--- src/main/java/com/vacinacao/agvacinacao/controller/UsuarioController.java
+++ src/main/java/com/vacinacao/agvacinacao/controller/UsuarioController.java
@@ -1,14 +1,19 @@
 package com.vacinacao.agvacinacao.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.vacinacao.agvacinacao.dto.UsuarioDTO;
 import com.vacinacao.agvacinacao.model.Usuario;
+import com.vacinacao.agvacinacao.repository.UsuarioRepository;
 import com.vacinacao.agvacinacao.service.UsuarioService;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,6 +22,9 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     // Endpoint para cadastrar novo usuário
     @PostMapping
@@ -30,6 +38,28 @@ public class UsuarioController {
     public ResponseEntity<Void> alterarSenha(@PathVariable Long id, @RequestBody String novaSenha) {
         usuarioService.alterarSenha(id, novaSenha);
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/recuperar-senha")
+    public ResponseEntity<?> recuperarSenha(@RequestBody Map<String, String> dados) {
+        String email = dados.get("email");
+        String novaSenha = dados.get("novaSenha");
+        String codigo = dados.get("codigo");
+
+        if (!"0000".equals(codigo)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Código inválido.");
+        }
+
+        Optional<Usuario> optionalUsuario = usuarioRepository.findByEmail(email);
+        if (optionalUsuario.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
+        }
+
+        Usuario usuario = optionalUsuario.get();
+        usuario.setSenha(new BCryptPasswordEncoder().encode(novaSenha));
+        usuarioRepository.save(usuario);
+
+        return ResponseEntity.ok("Senha atualizada com sucesso.");
     }
 
     // Endpoint para listar todos os usuários
